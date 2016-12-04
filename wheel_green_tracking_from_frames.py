6da665fffbbc9f71490b355e5526163bcea20195
philipperemy <premy@reactive.co.jp>
2016-12-04 09:23:49 +0100

    ball tracking is now working 2

--- wheel_green_tracking_from_frames.py
+++ wheel_green_tracking_from_frames.py
@@ -1,82 +1,106 @@
 # import the necessary packages
-import argparse
+import os
 from collections import deque
 
 import cv2
+import dill
 import imutils
 import numpy as np
 
 from utils import FRAMES_DIR, FrameIterator
 
-# define the lower and upper boundaries of the "green"
-# ball in the HSV color space, then initialize the
-# list of tracked points
-green_lower = (29, 86, 6)
-green_upper = (64, 255, 255)
-pts = deque(maxlen=64)
 
-frames_iterator = FrameIterator(FRAMES_DIR)
-for (frame, name) in frames_iterator.read_frames():
+def points_analysis(results):
+    x_range = np.array([r[0] for r in results])
+    np.array(x_range < np.percentile(x_range, 95), dtype=int)
 
-    # resize the frame, blur it, and convert it to the HSV
-    # color space
-    frame = imutils.resize(frame, width=600)
-    # blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # construct a mask for the color "green", then perform
-    # a series of dilations and erosions to remove any small
-    # blobs left in the mask
-    mask = cv2.inRange(hsv, green_lower, green_upper)
-    mask = cv2.erode(mask, None, iterations=2)
-    mask = cv2.dilate(mask, None, iterations=2)
+def analyze_video():
+    results = []
+    # define the lower and upper boundaries of the "green"
+    # ball in the HSV color space, then initialize the
+    # list of tracked points
+    green_lower = (29, 86, 6)
+    green_upper = (64, 255, 255)
+    pts = deque(maxlen=64)
 
-    # cv2.imshow("Mask", mask)
+    frames_iterator = FrameIterator(FRAMES_DIR)
+    for i, (frame, name) in enumerate(frames_iterator.read_frames()):
+        print(name)
+        # resize the frame, blur it, and convert it to the HSV
+        # color space
+        frame = imutils.resize(frame, width=600)
+        # blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # find contours in the mask and initialize the current
-    # (x, y) center of the ball
-    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-                            cv2.CHAIN_APPROX_SIMPLE)[-2]
-    center = None
+        # construct a mask for the color "green", then perform
+        # a series of dilations and erosions to remove any small
+        # blobs left in the mask
+        mask = cv2.inRange(hsv, green_lower, green_upper)
+        mask = cv2.erode(mask, None, iterations=2)
+        mask = cv2.dilate(mask, None, iterations=2)
 
-    # only proceed if at least one contour was found
-    if len(cnts) > 0:
-        # find the largest contour in the mask, then use
-        # it to compute the minimum enclosing circle and
-        # centroid
-        c = max(cnts, key=cv2.contourArea)
-        ((x, y), radius) = cv2.minEnclosingCircle(c)
-        M = cv2.moments(c)
-        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        # cv2.imshow("Mask", mask)
 
-        # only proceed if the radius meets a minimum size
-        if radius > 10:
-            # draw the circle and centroid on the frame,
-            # then update the list of tracked points
-            cv2.circle(frame, (int(x), int(y)), int(radius),
-                       (0, 255, 255), 2)
-            cv2.circle(frame, center, 5, (0, 0, 255), -1)
+        # find contours in the mask and initialize the current
+        # (x, y) center of the ball
+        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                                cv2.CHAIN_APPROX_SIMPLE)[-2]
+        center = None
 
-    # update the points queue
-    pts.appendleft(center)
-    # loop over the set of tracked points
-    for i in range(1, len(pts)):
-        # if either of the tracked points are None, ignore
-        # them
-        if pts[i - 1] is None or pts[i] is None:
-            continue
+        # only proceed if at least one contour was found
+        if len(cnts) > 0:
+            # find the largest contour in the mask, then use
+            # it to compute the minimum enclosing circle and
+            # centroid
+            c = max(cnts, key=cv2.contourArea)
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+            M = cv2.moments(c)
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
-        # otherwise, compute the thickness of the line and
-        # draw the connecting lines
-        thickness = int(np.sqrt(64 / float(i + 1)) * 2.5)
-        cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+            # only proceed if the radius meets a minimum size
+            if radius > 5:
+                # draw the circle and centroid on the frame,
+                # then update the list of tracked points
+                cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+                cv2.circle(frame, center, 5, (0, 0, 255), -1)
+                print(center, i)
+                results.append(center)
+        # update the points queue
+        pts.appendleft(center)
+        # loop over the set of tracked points
+        for i in range(1, len(pts)):
+            # if either of the tracked points are None, ignore
+            # them
+            if pts[i - 1] is None or pts[i] is None:
+                continue
 
-    # show the frame to our screen
-    cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1) & 0xFF
+            # otherwise, compute the thickness of the line and
+            # draw the connecting lines
+            thickness = int(np.sqrt(64 / float(i + 1)) * 2.5)
+            cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
 
-    # if the 'q' key is pressed, stop the loop
-    if key == ord("q"):
-        break
+        # show the frame to our screen
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
 
-cv2.destroyAllWindows()
+        # if the 'q' key is pressed, stop the loop
+        if key == ord("q"):
+            break
+
+    cv2.destroyAllWindows()
+    return results
+
+
+def start():
+    wheel_file = 'w_res.pkl'
+    if os.path.isfile(wheel_file):
+        r = dill.load(open(wheel_file, 'rb'))
+    else:
+        r = analyze_video()
+        dill.dump(r, open(wheel_file, 'wb'))
+    points_analysis(r)
+
+
+if __name__ == '__main__':
+    start()
