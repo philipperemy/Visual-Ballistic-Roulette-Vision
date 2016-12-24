5482e5d98c0bf0e3a3a03452f854c330dca8289c
philipperemy <premy@reactive.co.jp>
2016-12-24 19:35:42 +0100

    add new videos

--- run_python.py
+++ run_python.py
@@ -1,11 +1,18 @@
-from subprocess import call
+from glob import glob
+from subprocess import check_output
 
 
-def call_program(video=None):
-    if video is None:
-        video = 'videos/1_10.mov'
-    call(['./run.sh', video])
+def call_program(v=None):
+    if v is None:
+        v = 'videos/1_10.mov'
+    out = check_output(['./run.sh', v])
+    output_filename = v + '.txt'
+    with open(output_filename, 'wt', encoding='utf-8') as w:
+        print('-> {}'.format(output_filename))
+        w.write(out.decode('utf-8'))
 
 
 if __name__ == '__main__':
-    call_program()
+    videos = glob('videos/video_dec_24_deutsche_bordeaux/*.mp4')
+    for video in videos:
+        call_program(video)
