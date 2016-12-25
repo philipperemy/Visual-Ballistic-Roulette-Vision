b02df64107a4a83194fd53dacc10ce9d4380dfd9
philipperemy <premy@reactive.co.jp>
2016-12-25 11:32:52 +0100

    add natural sort for listing the videos

--- run_python.py
+++ run_python.py
@@ -1,6 +1,8 @@
 from glob import glob
 from subprocess import check_output
 
+from natural_sort import natural_keys
+
 
 def call_program(v=None):
     if v is None:
@@ -14,5 +16,6 @@ def call_program(v=None):
 
 if __name__ == '__main__':
     videos = glob('videos/video_dec_24_deutsche_bordeaux/*.mp4')
+    videos.sort(key=natural_keys)
     for video in videos:
         call_program(video)
